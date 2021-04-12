b63cd737be0680500ed1c625664fa4f59e516ca9
Rob-Yoo <jin12243@naver.com>
2021-04-12 21:04:12 +0900

    C_04

--- C_04/ex04/ft_putnbr_base.c
+++ C_04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 20:28:31 by jinyoo            #+#    #+#             */
-/*   Updated: 2021/04/07 18:07:49 by jinyoo           ###   ########.fr       */
+/*   Updated: 2021/04/12 21:03:27 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		ft_strlen(char *base)
 int		is_exception(char *base)
 {
 	int i;
+	int j;
 
 	i = 0;
 	if (*base == '\0')
@@ -33,42 +34,29 @@ int		is_exception(char *base)
 		return (1);
 	while (base[i])
 	{
-		if (base[i] == base[i + 1])
-			return (1);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j++])
+				return (1);
+		}
 		i++;
 	}
-	i = 0;
-	while (base[i])
+	i = -1;
+	while (base[++i])
 	{
 		if (base[i] == '+' || base[i] == '-')
 			return (1);
-		i++;
 	}
 	return (0);
 }
 
-void	write_base(int mod, char *base)
-{
-	int i;
-
-	i = 0;
-	while (base[i])
-	{
-		if (i == mod)
-			write(1, &base[i], 1);
-		i++;
-	}
-}
-
 void	change_base(int nbr, int base_num, char *base)
 {
-	if (nbr / base_num == 0)
-	{
-		write_base(nbr, base);
+	if (!nbr)
 		return ;
-	}
 	change_base(nbr / base_num, base_num, base);
-	write_base(nbr % base_num, base);
+	write(1, &base[nbr % base_num], 1);
 	return ;
 }
 
@@ -82,12 +70,12 @@ void	ft_putnbr_base(int nbr, char *base)
 	if (nbr < 0)
 	{
 		write(1, "-", 1);
-		change_base(-nbr / base_num, base_num, base);
-		write_base(-nbr % base_num, base);
+		change_base(-(nbr / base_num), base_num, base);
+		write(1, &base[-(nbr % base_num)], 1);
 	}
 	else
 	{
 		change_base(nbr / base_num, base_num, base);
-		write_base(nbr % base_num, base);
+		write(1, &base[nbr % base_num], 1);
 	}
 }
